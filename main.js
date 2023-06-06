3f851f03e5fc13494aa118fe72c068bd71240acb
Douglas.Mariano <douglas.mariano@estudante.ufjf.br>
2023-06-06 01:09:55 -0300

    modificado a funcao atualiza

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {itens, x} from "./estoque.js";
+import {itens, adicionar, remover} from "./estoque.js";
 
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
@@ -21,7 +21,7 @@ function envia(evento){
     evento.preventDefault();
     console.log('Formulário enviado');
     const a = form.valor.value;
-    adicionar(n);
+    adicionar(a);
     form.valor.value = "";
     form.valor.focus();
     atualiza();
@@ -30,6 +30,15 @@ function envia(evento){
 
 function atualiza(){
     const ol = document.querySelector('ol');
-    ol.innerHTML = `<li>${x.estado}</li>`;
+    ol.innerHTML = "";
+    const z = itens();
+
+    for(let i=0; i<itens.length;i++){
+        const li = document.createElement('li');
+        li.textContent = z[i];
+        ol.appendChild(li);
+    }
+
+    z.push("Boom");
 }
 
